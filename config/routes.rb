a37be13ed3c6384f5c8e7a1660004e866072391d
Hillary Charen <hillary.charen@gmail.com>
2017-12-06 19:41:50 -0500

    registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'proposals#index'
+  get 'proposal/show'
   resources :proposals do
     resources :submissions do
       resources :comments
     end
-end
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
