f34094c13ad41498095628606b070287d1cfe6de
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-12-07 14:30:16 -0500

    default img adjustments

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,10 +1,10 @@
 class Submission < ApplicationRecord
-  
+
   validates :user_id, presence: true
   belongs_to :user
   belongs_to :proposal
   has_many :comments, dependent: :destroy
-  has_attached_file :avatar, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/tomlol.jpg"
+  has_attached_file :avatar, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/default-avatar.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 
