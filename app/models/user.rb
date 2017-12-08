986fa5c9dc9d4269e66011b72d9947ffbe43ad0e
DALLAS WALKER PETERSON <53N531@GMAIL.COM>
2017-12-08 14:46:44 -0500

    fixing user profile edit shit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
 
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,5 +13,5 @@ class User < ApplicationRecord
 
   #Paperclip shit
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/default-avatar.jpg"
-
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
